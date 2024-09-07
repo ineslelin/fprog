3399a6ef22bc4653239a5f9756af6e95d79acc53
ineslelin <ines.l.lintner@gmail.com>
2024-09-07 20:03:53 +0200

    Edit example 2

--- CR1/EX2/example.cpp
+++ CR1/EX2/example.cpp
@@ -1,29 +1,72 @@
 #include <iostream>
 #include <cmath>
+#include <sstream>
 
-using namespace std;
-
-int a=0;
-int V=0;
-int A=0;
-float d=0;
-float ri=0;
-float ru=0;
-
-void wuerfel ()
+struct Cube
 {
-	V=pow(a,3);
-	A=6*pow(a,2);
-	d=a*sqrt(3);
-	ru=a/2*sqrt(3);
-	ri=a/2;
+	int a;
+};
+
+int Volume(Cube const& cube);
+int Surface(Cube const& cube);
+float Diagonal(Cube const& cube);
+float IncircleRadius(Cube const& cube);
+float CircumferenceRadius(Cube const& cube);
+
+std::string FormatOutput(int const& V, int const& A, float const& d, float const& ri, float const& ru);
+void PrintOutput(std::string const& text);
+
+int main (int argc, char *argv[])
+{
+  	Cube cube = { 2 };
+
+	int V = Volume(cube);
+	int A = Surface(cube);
+	float d = Diagonal(cube);
+	float ri = IncircleRadius(cube);
+	float ru = CircumferenceRadius(cube);
+
+	std::string output = FormatOutput(V, A, d, ri, ru);
+	PrintOutput(output);
+
+  	return 0;
 }
 
-int
-main (int argc, char *argv[])
+int Volume(Cube const& cube)
 {
-  a = 2;
-  wuerfel ();
-  cout << "Volumen:" << V << " Oberfläche:"<<A << " Diagonale:" << d << " Inkreisradius:" << ri << " Umkreisradius:" <<ru << endl;
-  return 0;
+	return pow(cube.a, 3);
+}
+
+int Surface(Cube const& cube)
+{
+	return 6 * pow(cube.a, 2);
+}
+
+float Diagonal(Cube const& cube)
+{
+	return cube.a * sqrt(3);
+}
+
+float IncircleRadius(Cube const& cube)
+{
+	return cube.a / 2;
+}
+
+float CircumferenceRadius(Cube const& cube)
+{
+	return cube.a / 2 * sqrt(3);
+}
+
+std::string FormatOutput(int const& V, int const& A, float const& d, float const& ri, float const& ru)
+{
+	std::stringstream output;
+
+	output << "Volumen: " << V << " Oberfläche: "<< A << " Diagonale: " << d << " Inkreisradius: " << ri << " Umkreisradius: " << ru << std::endl;
+
+	return output.str();
+}
+
+void PrintOutput(std::string const& text)
+{
+	std::cout << text;
 }
