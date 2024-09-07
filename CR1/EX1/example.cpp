90af0b15bb34521b87318f66dea6eea1283e94db
ineslelin <ines.l.lintner@gmail.com>
2024-09-07 19:14:41 +0200

    Edit example 1

--- CR1/EX1/example.cpp
+++ CR1/EX1/example.cpp
@@ -1,29 +1,66 @@
 #include <iostream>
 #include <cmath>
+#include <sstream>
 
-using namespace std;
+// using namespace std;
 
-int a=0;
-int b=0;
-int c=0;
-int V=0;
-int A=0;
-float d=0;
-
-void quader ()
+struct Quader
 {
-	V=a*b*c;
-	A=2*(a*b+a*c+b*c);
-	d=sqrt(pow(a,2)+pow(b,2)+pow(c,2));
+  int a;
+  int b;
+  int c;
+};
+
+int Volume(Quader const& quader);
+int Surface(Quader const& quader);
+float Diagonal(Quader const& quader);
+
+std::string FormatResults(int const& V, int const& A, float const& d);
+void PrintOutput(std::string const& text);
+
+int main(int argc, char *argv[])
+{
+  Quader quader = {2, 3, 4};
+
+  int V = 0;
+  int A = 0;
+  float d = 0;
+
+  V = Volume(quader);
+  A = Surface(quader);
+  d = Diagonal(quader);
+
+  std::string output = FormatResults(V, A, d);
+  PrintOutput(output);
+  
+  return 0;
 }
 
-int
-main (int argc, char *argv[])
+int Volume(Quader const& quader)
 {
-  a = 2;
-  b = 3;
-  c = 4;
-  quader ();
-  cout << "Volumen:" << V << " Oberfläche:"<<A << " Diagonale:" << d << endl;
-  return 0;
+  return quader.a * quader.b * quader.c;
+}
+
+int Surface(Quader const& quader)
+{
+  return 2 * (quader.a * quader.b + quader.a * quader.c + quader.b * quader.c);
+}
+
+float Diagonal(Quader const& quader)
+{
+  return sqrt(pow(quader.a, 2) + pow(quader.b, 2) + pow(quader.c, 2));
+}
+
+std::string FormatResults(int const& V, int const& A, float const& d)
+{
+  std::stringstream output;
+
+  output << "Volumen: " << V << " Oberfläche: " << A << " Diagonale: " << d << std::endl;
+
+  return output.str();
+}
+
+void PrintOutput(std::string const& text)
+{
+  std::cout << text;
 }
