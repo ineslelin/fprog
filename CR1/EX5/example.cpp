7423f4f8f15a1057d80a419a928ce9395d2627c3
ineslelin <ines.l.lintner@gmail.com>
2024-09-08 11:10:42 +0200

    Edit example 5

--- CR1/EX5/example.cpp
+++ CR1/EX5/example.cpp
@@ -1,27 +1,65 @@
 #include <iostream>
 #include <cmath>
+#include <sstream>
 
-using namespace std;
-
-int a=0;
-float V=0;
-float A=0;
-float ri=0;
-float ru=0;
-
-void ikosaeder ()
+struct Ikosaeder
 {
-	V=5*pow(a,3)*(3+sqrt(5))/12;
-	A=5*pow(a,2)*sqrt(3);
-	ru=a/4.0*sqrt(2*(5+sqrt(5)));
-	ri=a*sqrt(3)*(3+sqrt(5))/12;
+  int a;
+};
+
+float Volume(Ikosaeder const& ikosaeder);
+float Surface(Ikosaeder const& ikosaeder);
+float IncircleRadius(Ikosaeder const& ikosaeder);
+float CircumferenceRadius(Ikosaeder const& ikosaeder);
+
+std::string FormatOutput(float const& V, float const& A, float const& ri, float const& ru);
+void PrintOutput(std::string const& text);
+
+int main(int argc, char *argv[])
+{
+  Ikosaeder ikosaeder = { 2 };
+
+  float V = Volume(ikosaeder);
+  float A = Surface(ikosaeder);
+  float ri = IncircleRadius(ikosaeder);
+  float ru = CircumferenceRadius(ikosaeder);
+
+  std::string output = FormatOutput(V, A, ri, ru);
+  PrintOutput(output);
+
+  return 0;
 }
 
-int
-main (int argc, char *argv[])
+float Volume(Ikosaeder const& ikosaeder)
 {
-  a = 2;
-  ikosaeder ();
-  cout << "Volumen:" << V << " Oberfläche:"<<A << " Inkreisradius:" << ri << " Umkreisradius:" <<ru << endl;
-  return 0;
+  return 5 * pow(ikosaeder.a, 3) * (3 + sqrt(5)) / 12;
+}
+
+float Surface(Ikosaeder const& ikosaeder)
+{
+  return 5 * pow(ikosaeder.a, 2) * sqrt(3);
+}
+
+float IncircleRadius(Ikosaeder const& ikosaeder)
+{
+  return ikosaeder.a * sqrt(3) * (3 + sqrt(5)) / 12;
+}
+
+float CircumferenceRadius(Ikosaeder const& ikosaeder)
+{
+  return ikosaeder.a / 4.0 * sqrt(2 * (5+sqrt(5)));
+}
+
+std::string FormatOutput(float const& V, float const& A, float const& ri, float const& ru)
+{
+  std::stringstream output;
+ 
+  output << "Volumen: " << V << " Oberfläche: "<< A << " Inkreisradius: " << ri << " Umkreisradius: " << ru << std::endl;
+
+  return output.str();
+}
+
+void PrintOutput(std::string const& text)
+{
+  std::cout << text;
 }
