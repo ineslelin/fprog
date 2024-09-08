f94e76e922a2af0a8e51a63aef7a8148c8122e33
ineslelin <ines.l.lintner@gmail.com>
2024-09-08 11:16:59 +0200

    Edit example 6

--- CR1/EX6/example.cpp
+++ CR1/EX6/example.cpp
@@ -1,27 +1,65 @@
 #include <iostream>
 #include <cmath>
+#include <sstream>
 
-using namespace std;
-
-int a=0;
-float V=0;
-float A=0;
-float ri=0;
-float ru=0;
-
-void dodekaeder ()
+struct Dodecaeder
 {
-	V=pow(a,3)*(15+7*sqrt(5))/4.0;
-	A=3*pow(a,2)*sqrt(5*(5+2*sqrt(5)));
-	ru=a*sqrt(3)*(1+sqrt(5))/4.0;
-	ri=a*sqrt(10*(25+11*sqrt(5)))/20.0;
+  int a;
+};
+
+float Volume(Dodecaeder const& dodecaeder);
+float Surface(Dodecaeder const& dodecaeder);
+float IncircleRadius(Dodecaeder const& dodecaeder);
+float CircumferenceRadius(Dodecaeder const& dodecaeder);
+
+std::string FormatOutput(float const& V, float const& A, float const& ri, float const& ru);
+void PrintOutput(std::string const& text);
+
+int main(int argc, char *argv[])
+{
+  Dodecaeder dodecaeder = { 2 };
+
+  float V = Volume(dodecaeder);
+  float A = Surface(dodecaeder);
+  float ri = IncircleRadius(dodecaeder);
+  float ru = CircumferenceRadius(dodecaeder);
+
+  std::string output = FormatOutput(V, A, ri, ru);
+  PrintOutput(output);
+
+  return 0;
 }
 
-int
-main (int argc, char *argv[])
+float Volume(Dodecaeder const& dodecaeder)
 {
-  a = 2;
-  dodekaeder ();
-  cout << "Volumen:" << V << " Oberfläche:"<<A << " Inkreisradius:" << ri << " Umkreisradius:" <<ru << endl;
-  return 0;
+  return pow(dodecaeder.a, 3) * (15 + 7 * sqrt(5)) / 4.0;
+}
+
+float Surface(Dodecaeder const& dodecaeder)
+{
+  return 3 * pow(dodecaeder.a, 2) * sqrt(5 * (5 + 2 * sqrt(5)));
+}
+
+float IncircleRadius(Dodecaeder const& dodecaeder)
+{
+  return dodecaeder.a * sqrt(10 * (25 + 11 * sqrt(5))) / 20.0;
+}
+
+float CircumferenceRadius(Dodecaeder const& dodecaeder)
+{
+  return dodecaeder.a * sqrt(3) * (1 + sqrt(5)) / 4.0;
+}
+
+std::string FormatOutput(float const& V, float const& A, float const& ri, float const& ru)
+{
+  std::stringstream output;
+ 
+  output << "Volumen: " << V << " Oberfläche: "<< A << " Inkreisradius: " << ri << " Umkreisradius: " << ru << std::endl;
+
+  return output.str();
+}
+
+void PrintOutput(std::string const& text)
+{
+  std::cout << text;
 }
