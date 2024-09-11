ebe79bab6212ebca7febc660ba73136911e0afcc
ineslelin <ines.l.lintner@gmail.com>
2024-09-11 18:12:09 +0200

    Add task from moodle quiz 1

---
+++ moodle-quizzes/MQ1/functionalLoops.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+
+using namespace std;
+
+int prodWithFunctionalLoops(vector<int> const& numbers)
+{
+    return 0;
+}
+
+int main() {
+    vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8};
+    // Calculate and print the product of the vector
+    int prod = prodWithFunctionalLoops(numbers);
+    cout << "Product of the vector is " << prod << endl;
+
+    return 0;
+}
+
+// int main() {
+//     vector<int> numbers = {2, 4, 6, 8};
+//     // Calculate and print the product of the vector
+//     int prod = prodWithFunctionalLoops(numbers);
+//     cout << "Product of the vector is " << prod << endl;
+
+//     return 0;
+// }
+
+// int main() {
+//     vector<int> numbers = {8, 3, 1,};
+//     // Calculate and print the product of the vector
+//     int prod = prodWithFunctionalLoops(numbers);
+//     cout << "Product of the vector is " << prod << endl;
+
+//     return 0;
+// }
