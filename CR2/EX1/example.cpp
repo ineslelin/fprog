3dec6a32fbdfac4fc82c7dcc3138ca7fdc9ecf08
ineslelin <ines.l.lintner@gmail.com>
2024-09-14 15:59:55 +0200

    Edit example 1

--- CR2/EX1/example.cpp
+++ CR2/EX1/example.cpp
@@ -2,8 +2,6 @@
 #include <cmath>
 #include <sstream>
 
-// using namespace std;
-
 struct Quader
 {
   int a;
@@ -11,9 +9,9 @@ struct Quader
   int c;
 };
 
-int Volume(Quader const& quader);
-int Surface(Quader const& quader);
-float Diagonal(Quader const& quader);
+auto Volume = [](Quader const& quader) { return quader.a * quader.b * quader.c; };
+auto Surface = [](Quader const& quader) { return 2 * (quader.a * quader.b + quader.a * quader.c + quader.b * quader.c); };
+auto Diagonal = [](Quader const& quader) { return sqrt(pow(quader.a, 2) + pow(quader.b, 2) + pow(quader.c, 2)); };
 
 std::string FormatResults(int const& V, int const& A, float const& d);
 void PrintOutput(std::string const& text);
@@ -22,33 +20,14 @@ int main(int argc, char *argv[])
 {
   Quader quader = {2, 3, 4};
 
-  int V = 0;
-  int A = 0;
-  float d = 0;
-
-  V = Volume(quader);
-  A = Surface(quader);
-  d = Diagonal(quader);
+  auto V = Volume(quader);
+  auto A = Surface(quader);
+  auto d = Diagonal(quader);
 
   std::string output = FormatResults(V, A, d);
   PrintOutput(output);
   
   return 0;
-}
-
-int Volume(Quader const& quader)
-{
-  return quader.a * quader.b * quader.c;
-}
-
-int Surface(Quader const& quader)
-{
-  return 2 * (quader.a * quader.b + quader.a * quader.c + quader.b * quader.c);
-}
-
-float Diagonal(Quader const& quader)
-{
-  return sqrt(pow(quader.a, 2) + pow(quader.b, 2) + pow(quader.c, 2));
 }
 
 std::string FormatResults(int const& V, int const& A, float const& d)
