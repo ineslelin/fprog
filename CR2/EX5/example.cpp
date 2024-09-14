59a3df3bac5bf512891fa9b304510d652dc4a540
ineslelin <ines.l.lintner@gmail.com>
2024-09-14 16:54:45 +0200

    Edit example 5

--- CR2/EX5/example.cpp
+++ CR2/EX5/example.cpp
@@ -2,52 +2,27 @@
 #include <cmath>
 #include <sstream>
 
-struct Ikosaeder
-{
-  int a;
-};
-
-float Volume(Ikosaeder const& ikosaeder);
-float Surface(Ikosaeder const& ikosaeder);
-float IncircleRadius(Ikosaeder const& ikosaeder);
-float CircumferenceRadius(Ikosaeder const& ikosaeder);
+auto Volume = [](int const& a){ return 5 * pow(a, 3) * (3 + sqrt(5)) / 12; };
+auto Surface = [](int const& a){ return 5 * pow(a, 2) * sqrt(3); };
+auto IncircleRadius = [](int const& a){ return a * sqrt(3) * (3 + sqrt(5)) / 12; };
+auto CircumferenceRadius = [](int const& a){ return a / 4.0 * sqrt(2 * (5 + sqrt(5))); };
 
 std::string FormatOutput(float const& V, float const& A, float const& ri, float const& ru);
 void PrintOutput(std::string const& text);
 
 int main(int argc, char *argv[])
 {
-  Ikosaeder ikosaeder = { 2 };
+  int a = 2;
 
-  float V = Volume(ikosaeder);
-  float A = Surface(ikosaeder);
-  float ri = IncircleRadius(ikosaeder);
-  float ru = CircumferenceRadius(ikosaeder);
+  float V = Volume(a);
+  float A = Surface(a);
+  float ri = IncircleRadius(a);
+  float ru = CircumferenceRadius(a);
 
   std::string output = FormatOutput(V, A, ri, ru);
   PrintOutput(output);
 
   return 0;
-}
-
-float Volume(Ikosaeder const& ikosaeder)
-{
-  return 5 * pow(ikosaeder.a, 3) * (3 + sqrt(5)) / 12;
-}
-
-float Surface(Ikosaeder const& ikosaeder)
-{
-  return 5 * pow(ikosaeder.a, 2) * sqrt(3);
-}
-
-float IncircleRadius(Ikosaeder const& ikosaeder)
-{
-  return ikosaeder.a * sqrt(3) * (3 + sqrt(5)) / 12;
-}
-
-float CircumferenceRadius(Ikosaeder const& ikosaeder)
-{
-  return ikosaeder.a / 4.0 * sqrt(2 * (5+sqrt(5)));
 }
 
 std::string FormatOutput(float const& V, float const& A, float const& ri, float const& ru)
