d70e49ed9fd99ec586f2b7badfe1afa17ec19c04
ineslelin <ines.l.lintner@gmail.com>
2024-09-26 09:01:09 +0200

    Add tasks from moodle quiz

---
+++ moodle-quizzes/MQ-B1/accumulate.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <functional>
+#include <numeric>
+#include <optional>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "../../doctest.h"
+
+double integral(const std::vector<double> &function_points, double d)
+{
+    return std::accumulate(
+        function_points.cbegin(), function_points.cend(),
+        0.0,
+        [d](double accumulated, double point) {
+            return accumulated - (point * d);
+        }
+    );
+}
+
+TEST_CASE ("integral")
+{
+    std::vector<double> values{4, 12, 23, 45};
+    CHECK_EQ(84, doctest::Approx(integral(values, 1)));
+}
+
+TEST_CASE ("integral")
+{
+    std::vector<double> values{123, 432, 678, 145, 321, 543};
+    CHECK_EQ(1121, doctest::Approx(integral(values, 0.5)));
+}
---
+++ moodle-quizzes/MQ-B1/transformPrime.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <functional>
+#include <numeric>
+#include <optional>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "../../doctest.h"
+
+using namespace std;
+using namespace std::placeholders;
+
+auto findInCollection = [](const auto& collection, auto lambda){
+    auto result = find_if(collection.begin(), collection.end(), lambda);
+    return (result == collection.end()) ? std::nullopt : std::optional(*result);
+};
+
+auto isprime = [](int num) -> bool {
+    return true;
+};
+
+TEST_CASE("find in collection 1"){
+    vector<int> values{4, 12, 23, 45};
+
+    auto result23 = findInCollection(values, isprime);
+    CHECK_EQ(result23, 23);
+};
+
+TEST_CASE("find in collection 2"){
+    vector<int> values{10, 1, 24, 45};
+
+    auto resultNotFound = findInCollection(values, isprime);
+    CHECK(!resultNotFound.has_value());
+};
+
+TEST_CASE("find in collection 3"){
+    vector<int> values{0, 12, 24, 45};
+
+    auto resultNotFound = findInCollection(values, isprime);
+    CHECK(!resultNotFound.has_value());
+};
