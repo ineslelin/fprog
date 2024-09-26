1a1662c098f9cf7d7a88b2d6a6aabae69dce76e8
ineslelin <ines.l.lintner@gmail.com>
2024-09-26 14:19:04 +0200

    Finish accumulate

--- moodle-quizzes/MQ-B1/accumulate.cpp
+++ moodle-quizzes/MQ-B1/accumulate.cpp
@@ -11,18 +11,18 @@ double integral(const std::vector<double> &function_points, double d)
         function_points.cbegin(), function_points.cend(),
         0.0,
         [d](double accumulated, double point) {
-            return accumulated - (point * d);
+            return accumulated + (point * d);
         }
     );
 }
 
-TEST_CASE ("integral")
+TEST_CASE ("integral 1")
 {
     std::vector<double> values{4, 12, 23, 45};
     CHECK_EQ(84, doctest::Approx(integral(values, 1)));
 }
 
-TEST_CASE ("integral")
+TEST_CASE ("integral 2")
 {
     std::vector<double> values{123, 432, 678, 145, 321, 543};
     CHECK_EQ(1121, doctest::Approx(integral(values, 0.5)));
