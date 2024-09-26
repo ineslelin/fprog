0f2b7fe045a3ca8e883afed511e29dd85f2a768e
ineslelin <ines.l.lintner@gmail.com>
2024-09-26 14:14:17 +0200

    Finish transform prime

--- moodle-quizzes/MQ-B1/transformPrime.cpp
+++ moodle-quizzes/MQ-B1/transformPrime.cpp
@@ -13,7 +13,22 @@ auto findInCollection = [](const auto& collection, auto lambda){
     return (result == collection.end()) ? std::nullopt : std::optional(*result);
 };
 
-auto isprime = [](int num) -> bool {
+auto isprime = [](int const& num) -> bool {
+    if(num <= 1)
+        return false;
+
+    if(num == 2)
+        return true;
+
+    if(num % 2 == 0)
+        return false;
+
+    for(int i = 3; i * i <= num; i += 2)
+    {
+        if(num % i == 0)
+            return false;
+    }
+
     return true;
 };
 
