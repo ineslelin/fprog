9c35f160416eb2becef4f3a734a7a76d1bf8a235
ineslelin <ines.l.lintner@gmail.com>
2024-09-27 13:45:27 +0200

    Solve duplicateStrings

--- moodle-quizzes/MQ-B3/duplicateStrings.cpp
+++ moodle-quizzes/MQ-B3/duplicateStrings.cpp
@@ -24,9 +24,8 @@ TEST_CASE("Remove duplicates from a string (to edit, results in abab)")
 {
     string str = "aabbaaabbbbbb";
 
+    sort(str.begin(), str.end());
     auto res = remove_duplicates(str.begin(), str.end());
-
-    // code
 
     CHECK_EQ("ab",string(str.begin(), res));
 }
@@ -34,9 +33,9 @@ TEST_CASE("Remove duplicates from a string (to edit, results in abab)")
 TEST_CASE("Remove duplicates from a string (runs correctly)")
 {
     string str = "aaaaabbbbbb";
-    auto res = remove_duplicates(str.begin(), str.end());
 
-    // code
+    sort(str.begin(), str.end());
+    auto res = remove_duplicates(str.begin(), str.end());
 
     CHECK_EQ("ab",string(str.begin(), res));
 }
