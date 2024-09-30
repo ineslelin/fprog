96de38bbf523bebc683e8700d779a75619a0bbcd
ineslelin <ines.l.lintner@gmail.com>
2024-09-30 20:30:31 +0200

    Add error handling

--- CR-B3/bubbleSort.cpp
+++ CR-B3/bubbleSort.cpp
@@ -17,6 +17,9 @@ auto isListSorted = [](std::vector<float> const& list)
 
 auto sortList = [](std::vector<float> const& list) -> std::vector<float>
 {
+    if(list.size() <= 1)
+        return list;
+
     std::vector<float> unsortedList = list;
 
     std::function<void()> sortFunc = [&]() {
@@ -39,6 +42,24 @@ auto sortList = [](std::vector<float> const& list) -> std::vector<float>
     
     return unsortedList;
 };
+
+TEST_CASE("empty list")
+{
+    std::vector<float> unsorted = {};
+
+    std::vector<float> expected = {};
+
+    CHECK_EQ(expected, sortList(unsorted));
+}
+
+TEST_CASE("1 element")
+{
+    std::vector<float> unsorted = {1};
+
+    std::vector<float> expected = {1};
+
+    CHECK_EQ(expected, sortList(unsorted));
+}
 
 TEST_CASE("5 elements")
 {
