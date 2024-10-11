6e7d150a99645336b46d2349946ab4b8a9bf996f
ineslelin <ines.l.lintner@gmail.com>
2024-10-11 18:37:37 +0200

    Added examples

---
+++ moodle-quizzes/MQ-C/monadCorrect.cpp
@@ -0,0 +1,39 @@
+#include <experimental/optional>
+#include <iostream>
+#include <vector>
+
+std::experimental::optional < int > getFirst (const std::vector < int >&vec)
+{
+  if (!vec.empty ())
+    return std::experimental::optional < int >(vec[1]);
+  else
+    return std::experimental::optional < int >();
+} 
+
+int main ()
+{
+  std::vector < int > myVec { 1, 2, 3};
+  std::vector < int > myEmptyVec;
+  auto myInt = getFirst (myVec);
+  if (myInt)
+    {
+      std::cout << "*myInt: " << *myInt << std::endl;
+      std::cout << "myInt.value(): " << myInt.value () << std::endl;
+      std::cout << "myInt.value_or(2017):" << myInt.value_or (2017) << std:: endl;
+    }
+  std::cout << std::endl;
+  auto myEmptyInt = getFirst (myEmptyVec);
+  if (!myEmptyInt)
+    {
+      std:: cout << "myEmptyInt.value_or(2017):" << myEmptyInt.value_or (2017) << std::endl;
+    }
+}
+
+/* expected response
+
+    *myInt: 1
+    myInt.value(): 1
+    myInt.value_or(2017):1
+
+    myEmptyInt.value_or(2017):2017
+*/
---
+++ moodle-quizzes/MQ-C/monadTest.cpp
@@ -0,0 +1,55 @@
+#include <string>
+#include <functional>
+#include <iostream>
+#include <experimental/optional>
+
+// optional can be used as the return type of a factory that may fail
+std::experimental::optional<std::string> create(bool b) {
+   // your code here
+}
+ 
+// std::experimental::nullopt can be used to create any (empty) std::experimental::optional
+auto create2(bool b) {
+    // your code here
+}
+
+int main() // create(false) returned empty
+{
+    std::cout << "create(false) returned "
+              << create(false).value_or("empty") << '\n';
+    return 0;
+}
+
+// int main() // create2(true) returned Godzilla
+// {
+//     // optional-returning factory functions are usable as conditions of while and if
+//     if (auto str = create2(true)) {
+//         std::cout << "create2(true) returned " << *str << '\n';
+//     }
+//    return 0;
+
+// }
+
+// int main() // create(false) returned needed
+// {
+//     std::cout << "create(false) returned "
+//               << create(false).value_or("needed") << '\n';
+//     return 0;
+// }
+
+// int main() // create2(true) returned! Godzilla
+// {
+//     // optional-returning factory functions are usable as conditions of while and if
+//     if (auto str = create2(true)) {
+//         std::cout << "create2(true) returned! " << *str << '\n';
+//     }
+//    return 0;
+
+// }
+
+// int main() // create(true) returned Godzilla
+// {
+//     std::cout << "create(true) returned "
+//               << create(true).value_or("empty") << '\n';
+//     return 0;
+// }
